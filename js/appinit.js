a38bbc6953374ab6aa4077bfeeffc897b735b0de
almiradreamer <almira.mur@gmail.com>
2016-10-19 12:33:23 +0300

    Add files via upload

---
+++ js/appinit.js
@@ -0,0 +1,119 @@
+var app = angular.module("haul", []);
+
+/*app.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'partials/main.htm'
+		})
+		.when('/news', {
+			templateUrl: 'partials/news.htm'
+		})
+		.when('/world', {
+			templateUrl: 'partials/world.htm'
+		})
+	    .when('/profile', {
+	      templateUrl: 'partials/profile.htm'
+	    })
+	    .when('/likes', {
+	      templateUrl: 'partials/likes.htm'
+	    })
+	    .when('/login', {
+	      templateUrl: 'partials/login.htm'
+	    })
+	    .when('/signup', {
+	      templateUrl: 'partials/signup.htm'
+	    })
+}) 
+*/
+app.service('Server', function (AuthService) {
+  this.fetchGet = function (data, token) {
+    var address = 'http://192.241.144.134:8080/requests';
+    address += '?token=';
+    address += token;
+    address += '&data=';
+    address += data;
+    return fetch(address, {
+          method: 'get',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            
+          }
+        })
+          .then(function(response) { 
+            return response.json()
+          }).then(function(data) {
+            if (data.success == false) {
+              return AuthService.logout();
+            } else {
+              return data;
+            }
+          })
+  }
+  this.fetch = function (data, token) {
+    return fetch('https://alvolo.me:8080', {
+          method: 'post',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              data: data,
+              token: token
+          })
+        })
+          .then(function(response) { 
+            return response.json()
+          }).then(function(data) {
+            if (data.success == false) {
+              return AuthService.logout();
+            } else {
+              return data;
+            }
+          })
+  }
+})
+
+app.factory('AuthService', function ($rootScope, $window) {
+  var authService = {};
+  authService.logout = function() {
+    $window.localStorage.removeItem('token');
+    $window.localStorage.removeItem('userId');
+  }
+  authService.login = function (credentials) {
+    authService.logout();
+    fetch('http://192.241.144.134:8080/api/authenticate', {
+                    method: 'post',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        login: credentials.username,
+                        password: credentials.password
+                    })
+                }).then(function(response) {
+                    return response.json()
+                  }).then(function(text) { 
+                    if (text.success == true) {
+                      $window.localStorage['token'] = text.token;
+                      $window.localStorage['userId'] = text.id;
+                      $rootScope.$apply(function() {$rootScope.token = $window.localStorage['token'];
+                        $rootScope.userId = $window.localStorage['userId'];
+                        $rootScope.loginError = undefined;;
+                      })
+                    } 
+                    if (text.success == false) {
+                      $rootScope.$apply(function() {
+                        $rootScope.loginError = "Пользователь не найден";
+                      })
+                    }
+                  }); 
+    	return;
+	};
+	authService.isAuthenticated = function () {
+	   return !!$rootScope.token;
+	};
+	 
+	return authService;
+})
---
+++ js/controllers.js
@@ -0,0 +1,55 @@
+app.controller('mainCtrl', function($scope, $rootScope, $window, AuthService) {
+  $rootScope.token = $window.localStorage['token'];
+  $rootScope.userId = $window.localStorage['userId'];
+  
+  $scope.p = 4;
+  $scope.isLoggedIn = function () {
+    if ($window.localStorage['token']) {return true}
+      return false;
+  }
+  $scope.logout = function () {
+    
+    AuthService.logout();
+  }
+  $scope.$on('tokenExpired', function() {
+    $scope.logout();
+  })
+  if ($scope.isLoggedIn()) {
+    $rootScope.showingForm = 'news';
+  } else {
+    $rootScope.showingForm = 'global';
+  }
+})
+app.controller('loginCtrl', function($scope, $rootScope, AuthService, Server) {
+  $scope.login = function(credentials) {
+    AuthService.login(credentials)
+  }
+})
+app.controller('globalCtrl', function($scope, $rootScope, Server) {
+  $scope.data = '{"func":"getGlobalNews"}';
+  Server.fetch($scope.data, $rootScope.token).then(function(data) {
+    $scope.$apply(function() {
+      $scope.items = data;
+    })
+  })
+})
+app.controller('signupCtrl', function($scope, $rootScope, Server) {
+  $scope.signup = function() {
+    $scope.data = '{"func":"addUser","login":"';
+    $scope.data += $scope.login;
+    $scope.data += '", "pass":"';
+    $scope.data += $scope.password;
+    $scope.data += '", "role":"' + $scope.role;
+    $scope.data += '", "name":"' + $scope.name;
+    $scope.data += '", "surname":"' + $scope.surname;
+    $scope.data += '", "email":"' + $scope.email;
+    $scope.data += '", "photo":"' + $scope.photo + '"}';
+    Server.fetchSignUp($scope.data).then(function(data) {
+
+    })
+  }
+  
+})
+app.controller('newsCtrl', function($scope, $rootScope, Server) {
+  
+})
